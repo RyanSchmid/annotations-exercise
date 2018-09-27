9617f5c3c8b3df317e28a17bd4694de9d2379ed8
ryanschmid <schmid.ryan.c@gmail.com>
2018-09-27 11:39:05 -0500

    eliminated override error

--- src/Author.java
+++ src/Author.java
@@ -27,8 +27,7 @@ public class Author extends Person {
     public void addBook(String book) {
         books.add(book);
     }
-
-    @Override
+    
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
